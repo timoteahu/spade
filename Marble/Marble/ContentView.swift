d8019a78c84444cc200aa0979afe0156d84a0020
timoteahu <hutimk@gmail.com>
2024-07-04 01:03:18 -0700

    commit

---
+++ Marble/Marble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Marble
+//
+//  Created by Timothy Hu on 7/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Marble/Marble/MarbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarbleApp.swift
+//  Marble
+//
+//  Created by Timothy Hu on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MarbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
