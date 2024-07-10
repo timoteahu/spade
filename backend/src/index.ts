9be421a57e561997c1d841800b7d1ff4e8268fbf
ottokunkel <ottokunkel034@gmail.com>
2024-07-10 11:42:15 -0700

    move route, add to gitignore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,20 @@
 import express, { Request, Response } from "express";
+
 import UserRouter from "./routers/userRouter";
 
+/* setup express */
 const app = express();
-
 const port = 3000;
 
+/* go to routes */
+app.use("/user", UserRouter);
+
+/* frontend/other */
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript, ESLint, Prettier, and Express!");
 });
 
+/* begin listening */
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
-app.use("/user", UserRouter);
