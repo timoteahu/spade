e16957c31a94a20488c2ae6e9072ecb817597c52
timoteahu <hutimk@gmail.com>
2024-07-12 14:26:32 -0700

    change api-routes

--- backend/src/routers/groupRouter.ts
+++ backend/src/routers/groupRouter.ts
@@ -1,12 +1,13 @@
-import express from 'express'
-import * as groups from '../controllers/groupController'
+import express from "express";
+
+import * as groups from "../controllers/groupController";
 
 const router = express.Router();
 
-router.post("/create", groups.createGroup);
-router.get("/:userId/get-groups", groups.getGroups);
-router.get("/:groupId/join-group", groups.joinGroup);
-router.get("/:groupId/leave-group", groups.leaveGroup)
-router.get("/:groupId/change-group-name", groups.changeGroupName)
+router.post("/", groups.createGroup);
+router.get("/:userId", groups.getGroups);
+router.get("/join/:groupId", groups.joinGroup);
+router.get("/leave/:groupId", groups.leaveGroup);
+router.get("/change-name/:groupId", groups.changeGroupName);
 
-export default router
+export default router;
